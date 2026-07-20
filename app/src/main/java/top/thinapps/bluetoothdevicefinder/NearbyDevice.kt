cfcf102e43a064e599dcaca6d858f0f6aba47881
Jesse Nickles <jesse@littlebizzy.com>
2026-07-21 00:47:28 +0700

    Make scan result models immutable

--- app/src/main/java/top/thinapps/bluetoothdevicefinder/NearbyDevice.kt
+++ app/src/main/java/top/thinapps/bluetoothdevicefinder/NearbyDevice.kt
@@ -2,9 +2,9 @@ package top.thinapps.bluetoothdevicefinder
 
 data class NearbyDevice(
     val address: String,
-    var name: String,
-    var latestRssi: Int,
-    var smoothedRssi: Double,
-    var lastSeenElapsedTime: Long,
-    var paired: Boolean
+    val name: String,
+    val latestRssi: Int,
+    val smoothedRssi: Double,
+    val lastSeenElapsedTime: Long,
+    val paired: Boolean
 )
