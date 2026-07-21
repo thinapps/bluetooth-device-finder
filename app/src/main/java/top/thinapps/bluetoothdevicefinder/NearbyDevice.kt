4996972deb4406e5476b784958078b52cac07a7c
Jesse Nickles <jesse@littlebizzy.com>
2026-07-21 15:07:59 +0700

    Remove unused raw RSSI value

--- app/src/main/java/top/thinapps/bluetoothdevicefinder/NearbyDevice.kt
+++ app/src/main/java/top/thinapps/bluetoothdevicefinder/NearbyDevice.kt
@@ -3,7 +3,6 @@ package top.thinapps.bluetoothdevicefinder
 data class NearbyDevice(
     val address: String,
     val name: String,
-    val latestRssi: Int,
     val smoothedRssi: Double,
     val lastSeenElapsedTime: Long,
     val paired: Boolean
